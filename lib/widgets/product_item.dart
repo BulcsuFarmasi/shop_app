ee9ba33d3796a52038e3bd4a281aaf20667fdb83
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-03-09 14:18:52 +0100

    styling to product_item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,16 +9,32 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        title: Text(title, textAlign: TextAlign.center,),
-        leading: IconButton(icon: Icon(Icons.favorite), onPressed: () {},),
-        trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {},),
+    final theme = Theme.of(context);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: theme.accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: theme.accentColor,
+          ),
+        ),
       ),
     );
   }
