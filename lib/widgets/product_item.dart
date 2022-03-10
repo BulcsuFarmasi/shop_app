e01cb12887523449edf711a65cf7de67b31bf557
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-03-10 16:58:49 +0100

    use consumer in some places

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,13 +4,12 @@ import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final navigator = Navigator.of(context);
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
+    print('product rebuilds');
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
       child: GridTile(
@@ -29,10 +28,12 @@ class ProductItem extends StatelessWidget {
             product.title,
             textAlign: TextAlign.center,
           ),
-          leading: IconButton(
-            icon: Icon((product.isFavorite) ? Icons.favorite : Icons.favorite_border),
-            onPressed: product.toggleFavoriteStatus,
-            color: theme.accentColor,
+          leading: Consumer<Product>(
+            builder: (BuildContext ctx, Product product, _) => IconButton(
+              icon: Icon((product.isFavorite) ? Icons.favorite : Icons.favorite_border),
+              onPressed: product.toggleFavoriteStatus,
+              color: theme.accentColor,
+            ),
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
