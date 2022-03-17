c49bdd69e44117a543853b1bc6a6d6266d1815af
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-03-17 09:07:22 +0100

    add cart class and the provider for it

---
+++ lib/providers/cart.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({required this.id, required this.title, required this.quantity, required this.price});
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items => {..._items};
+
+  void addItem(String productId, double price, String title) {
+    _items.update(
+      productId,
+      (CartItem item) => CartItem(
+        id: item.id,
+        title: item.title,
+        quantity: item.quantity + 1,
+        price: item.price,
+      ),
+      ifAbsent: () => CartItem(
+        id: DateTime.now().toString(),
+        title: title,
+        quantity: 1,
+        price: price,
+      ),
+    );
+  }
+}
