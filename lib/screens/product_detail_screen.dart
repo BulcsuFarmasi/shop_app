faadfd25c5dd9510869ed755ccd7f4a4f8f0b18f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-04-05 10:30:35 +0200

    use slivers

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,44 +10,48 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
     final product = Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(product.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 300,
-                child: Hero(
-                  tag: product.id!,
-                  child: Image.network(
-                    product.imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(product.title),
+            background: Hero(
+              tag: product.id!,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                '\$${product.price}',
-                style: TextStyle(color: Colors.grey, fontSize: 20),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Text(
-                  product.description,
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                ),
-              )
-            ],
+            ),
           ),
-        ));
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${product.price}',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+            SizedBox(height: 800,)
+          ]),
+        )
+      ],
+    ));
   }
 }
